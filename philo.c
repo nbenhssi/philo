67ae8e7d3aa1ec7cd547e4dc252886c431d43ebe
Noura <nourabenhssine10@gmail.com>
2025-06-12 23:19:21 +0100

    Update philo.c

--- philo.c
+++ philo.c
@@ -1,84 +1,116 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nbenhssi <nbenhssi@student.1337.ma>        #+#  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-04-19 18:16:30 by nbenhssi          #+#    #+#             */
-/*   Updated: 2025-04-19 18:16:30 by nbenhssi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "philo.h"
-
-
-print_fct(t_data *a, int x)
-{
-    if (x = 0)
-        printf("Philo %d id eating", *a->philos.id);
-    else if (x == 1)
-        printf("Philo %d id sleeping", *a->philos.id);
-    else if (x == 2)
-        printf("Philo %d id thinking", *a->philos.id);
-}
 
 void *thread_function(void *p)
 {
-    static int j;
-	t_data *l;
-	int i;
-	l = (t_data *)p;
-    //routine
-    i = 0;
-    while(l)
+    t_philo *philo;
+    t_data *data;
+    
+    philo = (t_philo *)p;
+    data = philo->data;
+
+    if (philo->id % 2 != 0)
+        usleep(100);
+    while(1)
     {
-        if (l->nb_ph%2 == 0)
+        if (philo->data->stop == 1)
+            return NULL;
+        if (philo->id % 2 == 0)
         {
-            pthread_mutex_lock(&l->forks[i]);
-            //pthread_mutex_lock(l->meal);
-            pthread_mutex_lock(&l->forks[i+1]);
+            pthread_mutex_lock(philo->left_fork);
+            print_status(data, philo->id, "get left fork");
+            pthread_mutex_lock(philo->right_fork);
+            print_status(data, philo->id, "get right fork");
         }
-        i++;
+        else
+        {
+            pthread_mutex_lock(philo->right_fork);
+            print_status(data, philo->id, "get right fork");
+            pthread_mutex_lock(philo->left_fork);
+            print_status(data, philo->id, "get left fork");
+        }
+        pthread_mutex_lock(&philo->mael);
+        print_status(data, philo->id, "is eating");
+        philo->last_meal = get_time_ms();
+        pthread_mutex_unlock(&philo->mael);
+
+        smart_sleep(data->tte, data);
+        pthread_mutex_lock(&philo->mael_count);
+        philo->meal_count++;
+        pthread_mutex_unlock(&philo->mael_count);
+        
+        pthread_mutex_unlock(philo->right_fork);
+        pthread_mutex_unlock(philo->left_fork);
+        print_status(data, philo->id, "is sleeping");
+        smart_sleep(data->tts, data);
+        print_status(data, philo->id, "is thinking");
     }
-    printf("Hii im thread nbr %d\n", j);
-    j++;
     return(NULL);
 }
 
-void create_mutex(t_data *a)
+void *monitor_routine(void *arg)
 {
+    t_data *data;
     int i;
-    pthread_mutex_t	mutex[a->nb_ph];
-    i = 0;
-    while (i < a->nb_ph)
+    int all_ate;
+
+    data = (t_data *)arg;
+    while (1)
     {
-        pthread_mutex_init(&mutex[i], NULL);
-        i++;
+        i = 0;
+        all_ate = 1;
+        while (i < data->nb_ph)
+        {
+            if (get_time_ms() - data->philos[i].last_meal > data->ttd)
+            {
+                print_status(data, data->philos[i].id, "died");
+                data->stop = 1;
+                return (NULL);
+            }
+
+            if (data->n_o_t_e_ph_m_e != -1 && 
+                data->philos[i].meal_count < data->n_o_t_e_ph_m_e)
+                all_ate = 0;
+            i++;
+        }
+        if (data->n_o_t_e_ph_m_e != -1 && all_ate)
+        {
+            pthread_mutex_lock(&data->print_mutex);
+            data->stop = 1;
+            printf("All philosophers have eaten!\n");
+            pthread_mutex_unlock(&data->print_mutex);
+            return (NULL);
+        }
+        usleep(1000);
     }
+    return (NULL);
 }
 
 int create_philo(t_data *a)
 {
     int i = 0;
+	pthread_t monitor;
+
     while (i < a->nb_ph)
     {
-       // a->philos = malloc(sizeof(int));
-        *(a->philos->id) = i++;
-        if (pthread_create(&a->thread[i], NULL, thread_function, (void*)a) != 0)
+        if (pthread_create(&a->threads[i], NULL, thread_function, &a->philos[i]) != 0)
         {
             perror("pthread_create failed");
-            //free(a->philos->id[i]);
             return 1;
         }
         i++;
     }
+	if (pthread_create(&monitor, NULL, monitor_routine, a) != 0)
+    {
+        printf("Error: Failed to create monitor thread\n");
+        return (1);
+    }
     i = 0;
     while (i < a->nb_ph)
     {
-        pthread_join(a->thread[i], NULL);
+        pthread_join(a->threads[i], NULL);
         i++;
     }
+	pthread_join(monitor, NULL);
     return (0);
 }
 
@@ -86,15 +118,27 @@ int main(int argc, char **argv)
 {
     t_data a;
 
-
     if (argc != 5 && argc != 6)
     {
         printf("nbr of arg not valid");
-        return (0);
+        return (1);
     }
-	init_data(&a, argv, argc);
-	create_mutex(&a);
+	if (init_data(&a, argv, argc) != 0)
+    {
+        printf("Error: Failed to initialize data\n");
+        return (1);
+    }
+	if (init_mutex(&a) != 0)
+    {
+        printf("Error: Failed to initialize mutexes\n");
+        free_data(&a);
+        return (1);
+    }
 	init_philo(&a);
-	create_philo(&a);
-    //free();
+	if (create_philo(&a) != 0)
+    {
+        free_data(&a);
+        return (1);
+    }
+    free_data(&a);
 }
