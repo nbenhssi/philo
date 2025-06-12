9959af15ebaeec6cef4eec913ccd217b83728407
Noura <nourabenhssine10@gmail.com>
2025-06-13 00:30:50 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -28,8 +28,15 @@ void smart_sleep(long long duration, t_data *data)
     // (void)data;
 
     long start = get_time_ms();
-    while (!data->stop && get_time_ms() - start < duration)
-        usleep(500);
+    while (!data->stop)
+    {
+        if (get_time_ms() - start >= duration)
+            break;
+        usleep(200);
+    }
+    //     usleep(500);
+    // while (!data->stop && get_time_ms() - start < duration)
+    //     usleep(500);
     // start = get_time_ms();
     // while (1)
     // {
@@ -42,7 +49,7 @@ void smart_sleep(long long duration, t_data *data)
 void free_data(t_data *data)
 {
     int i;
-    
+
     i = 0;
     while (i < data->nb_ph)
     {
@@ -53,4 +60,29 @@ void free_data(t_data *data)
     free(data->philos);
     free(data->forks);
     free(data->threads);
+}
+
+void ft_usleep(long ms, t_data *data)
+{
+    long start;
+    long sleep_interval;
+    int should_continue;
+
+    start = get_time_ms();
+    if (data->nb_ph > 100)
+        sleep_interval = 500;
+    else if (data->nb_ph > 50)
+        sleep_interval = 100;
+    else
+        sleep_interval = 50;
+
+    while ((get_time_ms() - start) < ms)
+    {
+        pthread_mutex_lock(&data->death_mutex);
+        should_continue = !data->stop;
+        pthread_mutex_unlock(&data->death_mutex);
+        if (!should_continue)
+            break;
+        usleep(sleep_interval);
+    }
 }
