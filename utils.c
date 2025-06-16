a57b3cc2a80b83048ab1a665722a100df2e45fa3
Noura <nourabenhssine10@gmail.com>
2025-06-16 14:41:41 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -22,6 +22,7 @@ void print_status(t_data *data, int id, char *status)
     pthread_mutex_unlock(&data->print_mutex);
 }
 
+
 void smart_sleep(long long duration, t_data *data)
 {
     // long long start;
@@ -44,22 +45,6 @@ void smart_sleep(long long duration, t_data *data)
     //         break;
     //     usleep(100);
     // }
-}
-
-void free_data(t_data *data)
-{
-    int i;
-
-    i = 0;
-    while (i < data->nb_ph)
-    {
-        pthread_mutex_destroy(&data->forks[i]);
-        i++;
-    }
-    pthread_mutex_destroy(&data->print_mutex);
-    free(data->philos);
-    free(data->forks);
-    free(data->threads);
 }
 
 void ft_usleep(long ms, t_data *data)
@@ -86,3 +71,132 @@ void ft_usleep(long ms, t_data *data)
         usleep(sleep_interval);
     }
 }
+
+
+void free_data(t_data *data)
+{
+    int i = 0;
+    while (i < data->nb_ph)
+    {
+        pthread_mutex_destroy(&data->forks[i]);
+        i++;
+    }
+    i = 0;
+    while (i < data->nb_ph)
+    {
+        pthread_mutex_destroy(&data->philos[i].meal);
+        pthread_mutex_destroy(&data->philos[i].meal_count);
+        i++;
+    }
+    pthread_mutex_destroy(&data->print_mutex);
+    pthread_mutex_destroy(&data->death_mutex);
+    pthread_mutex_destroy(&data->stop_mutex);
+    if (data->philos)
+        free(data->philos);
+    if (data->forks)
+        free(data->forks);
+    if (data->threads)
+        free(data->threads);
+}
+
+// #include "philo.h"
+
+
+// long long get_time_ms(void)
+// {
+//     struct timeval tv;
+    
+//     gettimeofday(&tv, NULL);
+//     return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+// }
+
+// void print_status(t_data *data, int id, char *status)
+// {
+//     int should_stop;
+    
+//     pthread_mutex_lock(&data->stop_mutex);
+//     should_stop = data->stop;
+//     pthread_mutex_unlock(&data->stop_mutex);
+    
+//     if (should_stop)
+//         return;
+        
+//     pthread_mutex_lock(&data->print_mutex);
+//     printf("%lld ", get_time_ms() - data->start_time);
+//     printf("Philosopher %d %s\n", id + 1, status);
+//     pthread_mutex_unlock(&data->print_mutex);
+// }
+
+
+// void smart_sleep(long long duration, t_data *data)
+// {
+//     long start = get_time_ms();
+//     int should_stop;
+    
+//     while (1)
+//     {
+//         pthread_mutex_lock(&data->stop_mutex);
+//         should_stop = data->stop;
+//         pthread_mutex_unlock(&data->stop_mutex);
+        
+//         if (should_stop)
+//             break;
+            
+//         if (get_time_ms() - start >= duration)
+//             break;
+            
+//         usleep(200);
+//     }
+// }
+
+// void ft_usleep(long ms, t_data *data)
+// {
+//     long start;
+//     long sleep_interval;
+//     int should_continue;
+
+//     start = get_time_ms();
+//     if (data->nb_ph > 100)
+//         sleep_interval = 500;
+//     else if (data->nb_ph > 50)
+//         sleep_interval = 100;
+//     else
+//         sleep_interval = 50;
+
+//     while ((get_time_ms() - start) < ms)
+//     {
+//         pthread_mutex_lock(&data->stop_mutex);
+//         should_continue = !data->stop;
+//         pthread_mutex_unlock(&data->stop_mutex);
+//         if (!should_continue)
+//             break;
+//         usleep(sleep_interval);
+//     }
+// }
+
+
+// void free_data(t_data *data)
+// {
+//     int i = 0;
+//     while (i < data->nb_ph)
+//     {
+//         pthread_mutex_destroy(&data->forks[i]);
+//         i++;
+//     }
+//     i = 0;
+//     while (i < data->nb_ph)
+//     {
+//         pthread_mutex_destroy(&data->philos[i].meal);
+//         pthread_mutex_destroy(&data->philos[i].meal_count);
+//         i++;
+//     }
+//     pthread_mutex_destroy(&data->print_mutex);
+//     pthread_mutex_destroy(&data->death_mutex);
+//     pthread_mutex_destroy(&data->stop_mutex);
+//     if (data->philos)
+//         free(data->philos);
+//     if (data->forks)
+//         free(data->forks);
+//     if (data->threads)
+//         free(data->threads);
+// }
